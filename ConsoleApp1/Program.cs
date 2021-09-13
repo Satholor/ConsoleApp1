76bb8b4dbc1c06ee3ff4be197994ddedd5310f68
nicks <nicks@DESKTOP-TF0HQCT>
2021-09-13 16:53:10 -0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //  Console.WriteLine("Hello World!");
+            Console.WriteLine("          *");
+            Console.WriteLine("         * *");
+            Console.WriteLine("        *   *");
+            Console.WriteLine("       **   **");
+            Console.WriteLine("        *   *");
+            Console.WriteLine("      **     **");
+            Console.WriteLine("     ***     ***");
+            Console.WriteLine("       *     *");
+            Console.WriteLine("     **       **");
+            Console.WriteLine("   *****     *****");
+            Console.WriteLine("        *   *");
+            Console.WriteLine("        *   *");
+            Console.WriteLine("        *   *");
+            Console.WriteLine("        *****");
+            Console.ReadLine(); 
+
+        }
+    }
+}
